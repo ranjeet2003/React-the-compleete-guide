1697946b758420f71874705ec0b707529374e0cb
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-08 01:52:46 +0530

    deleting with key

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'asdsad1',name: 'Max', age: 28 },
+      { id: 'aadsas',name: 'Manu', age: 29 },
+      { id: 'asdas3',name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -52,7 +52,7 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person click = {() => this.deletePersonHandler(index)} name = {person.name} age = {person.age} />
+            return <Person click = {() => this.deletePersonHandler(index)} name = {person.name} age = {person.age} key = {person.id} />
           })}
         </div>
       );

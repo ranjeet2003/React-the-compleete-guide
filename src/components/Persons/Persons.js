c693c51eb127d0dddea8979c663c5d858f3c0193
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-14 12:34:08 +0530

    implementing PureComponent

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
   // static getDerivedStateFromProps(props, state) {
   //   console.log('[Persons.js] getDerivedStateFromProps');
@@ -12,15 +12,15 @@ class Persons extends Component {
     console.log('[Person.js] componentWillRecieveProps', props);
   }
 
-  shouldComponentUpdate( nextProps, nextState ) {
-    console.log('[Persons.js] shouldComponentsUpdate');
-    if(nextProps.persons !== this.props.persons) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
+  // shouldComponentUpdate( nextProps, nextState ) {
+  //   console.log('[Persons.js] shouldComponentsUpdate');
+  //   if(nextProps.persons !== this.props.persons || nextProps.changed !== this.props.changed || nextProps.clicked !==this.props.clicked) {
+  //     return true;
+  //   }
+  //   else {
+  //     return false;
+  //   }
+  // }
 
   getSnapshotBeforeUpdate( prevProps, prevState ) {
     console.log('[Persons.js] getSnapshotBeforeUpdate');

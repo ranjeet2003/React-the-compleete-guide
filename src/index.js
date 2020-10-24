c63356421e256b3422e05edf572ad01d1072747b
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-24 21:05:53 +0530

    Adding interceptor to execute code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use( Response=> {
+    console.log(Response);
+    return Response;
+},error =>{
+    console.log(error);
+    return Promise.reject.error;
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();

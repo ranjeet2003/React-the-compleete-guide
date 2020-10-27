15f3447b2b77c83d6efad2c6e7a4d79987d9e7a6
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-28 01:09:40 +0530

    Switching between pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 // import axios from 'axios';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -20,6 +21,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={()=><h1>Home</h1>} />
                 <Route path="/"  render={()=><h1>Home 2</h1>} /> */}
                 <Route  path="/" exact component ={Posts} />
+                <Route  path="/new-post" component ={NewPost} />
             </div>
         );
     }

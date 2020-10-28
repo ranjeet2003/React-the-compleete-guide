fd6f5b1f8890a1e1d178f21de76b720d7855f96a
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-28 16:26:18 +0530

    Using Switch to load single route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route,  NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 // import axios from 'axios';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -14,15 +14,15 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink 
-                                to = "/" 
-                                exact 
+                            <li><NavLink
+                                to="/"
+                                exact
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
                                 }}  >Home</NavLink></li>
-                            <li><NavLink to = {{
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit = true'
@@ -32,9 +32,11 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={()=><h1>Home</h1>} />
                 <Route path="/"  render={()=><h1>Home 2</h1>} /> */}
-                <Route  path="/" exact component ={Posts} />
-                <Route  path="/new-post" component ={NewPost} />
-                <Route  path="/:id" component ={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" component={FullPost} />
+                </Switch>
             </div>
         );
     }
